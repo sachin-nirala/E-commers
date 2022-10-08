51f34f85acd13ce00c304eee4988001ea2126cce
sachin-nirala <106684019+sachin-nirala@users.noreply.github.com>
2022-10-08 19:20:20 +0530

    second init

--- ecomproject/wsgi.py
+++ ecomproject/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for ecomproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, seee
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
